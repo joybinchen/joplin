eb4d0c5f407240d38e54f1d5187eb7fb7c88bab6
Joybin Chen <6048003+joybinchen@users.noreply.github.com>
2019-01-23 14:26:54 +0800

    Desktop & CliClient: rest Api downloadImages_ get correct filename to save as resource.title for http url.

--- CliClient/tests/services_rest_Api_images.js
+++ CliClient/tests/services_rest_Api_images.js
@@ -197,8 +197,9 @@ const testImageType = async (done, filePath, contentType, fileExt, mime, fileNam
 		expect(response.body.indexOf('(:/'+resource.id+')') >= 0).toBe(true);
 
 		expect(Resource.isSupportedImageMimeType(resource.mime)).toBe(supportedImage);
-	} else {
-		logger.debug('receive', imageUrl, resources.length ? resources[0] : 'none');
+		expect(resource.mime).toBe(mime);
+		expect(resource.file_extension).toBe(fileExt);
+		if (fileName) expect(resource.title).toBe(fileName);
 	}
 	done();
 };
@@ -254,4 +255,25 @@ describe('services_rest_Api should load correctly for', function() {
 		testImageWithContentType('supported', filePath, supported[filePath]);
 		testImageWithContentType('supported', filePath, 'application/octet-stream');
 	}
+
+	const misleading = {
+		'../../Assets/JoplinLetter.svg': 'image/jpeg',
+		'../../Assets/Adresse.png': 'image/jpg',
+		'../tests/support/photo.jpg': 'image/png',
+		'../tests/support/slow-typing-doc1.gif': 'image/jpeg',
+		'../tests/support/webp-animated.webp': 'image/png',
+	};
+	for(let filePath in misleading) if (filePath) {
+		testImageWithContentType('misleading', filePath, misleading[filePath]);
+	}
+
+	const nonImage = {
+		'../tests/support/jasmine.json': 'application/json',
+		'../../Tools/PortableAppsLauncher/App/readme.txt': 'text/plain',
+		// 'images/Joplin.ico': 'image/vnd.microsoft.icon',
+	};
+	for(let filePath in nonImage) if (filePath) {
+		testImageWithContentType('unsupported', filePath, nonImage[filePath], false);
+	}
+
 });
